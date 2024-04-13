7e961860fc283a002afbada646a11933c702f224
Dasradhv <dasaradh_s@yahoo.com>
2024-04-13 18:35:12 +0530

    Add LICENSE.txt.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Docker World!, World!");
+
+var class1 = new Exception();
+
+//class1.Message = "TEST MESSAGE";
+
+Console.WriteLine(class1.Message.ToString());
